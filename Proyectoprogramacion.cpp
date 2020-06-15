79262a3b7a57ed279b830058f4d2f2d02fbfa4d8
Ariel-r03 <66917994+Ariel-r03@users.noreply.github.com>
2020-06-14 21:40:05 -0600

    Add files via upload
    Funciones de los textos

--- Proyectoprogramacion.cpp
+++ Proyectoprogramacion.cpp
@@ -1,10 +1,50 @@
 #include<cstdio>
-
-
-
+#include <iostream>
+#include <string.h>
+#include <cstring>
+#include<cctype>
+using namespace std;
+void Romanos();
+void Juan_3_18();
+void Jeremias21_8();
+void Mateo7_13();
+void Juan11_25_27();
+void Mateo7_14();
+void Deuteronomio_30_19();
 int main()
 {
-	
-	
+Deuteronomio_30_19();
 	return 0;
+}
+
+void Romanos(){
+cout<<"Que si confiesas con tu boca que Jesus es el senor y crees que tu corazon Dios lo levanto de entre los muertos";
+cout<<" seras salvo. Porque con el corazon se cree para ser justificado, pero con la boca se confiesa para ser salvo." <<endl;
+}
+
+void Juan_3_18(){
+	cout<<" El que cree en mi, que soy el Hijo de Dios, no sera condenado por Dios. Pero el que no cree ya ha sido condenado,";
+	cout<<" precisamente por no haber reido en el Hijo unico de Dios. "<<endl;
+}
+
+void Jeremias21_8(){
+	cout<<"Y a este pueblo adviertele que asi dice el Senor: Pongo delante de ustedes el camino de la vida y el camino de la muerte."<<endl; 
+}
+
+void Mateo7_13(){
+	cout<<"Entrad por la puerta estrecha; porque ancha es la puerta, y espacioso el camino que lleva a la perdicion, y muchos son los que entran por ella"<<endl;
+}
+
+void Juan11_25_27(){
+cout<<" Le dijo Jesus: Yo soy la resurreccion y la vida; el que cree en mi, aunque este muerto, vivira. Y todo aquel que vive y cree en mi, no morira eternamente. Crees esto?";
+cout<<"Le dijo: Si, Senor; yo he creido que tu eres el Cristo, el Hijo de Dios, que has venido al mundo."<<endl;
+}
+
+void Mateo7_14(){
+	cout<<"Porque estrecha es la puerta, y angosto el camino que lleva a la vida, y pocos son los que la hallan."<<endl;
+}
+
+void Deuteronomio_30_19(){
+	cout<<"Hoy pongo al cielo y a la tierra por testigos contra ti, de que te he dado a elegir entre la vida y la muerte,";
+	cout<<"entre la bendicion y la maldicion. Elige, pues, la vida,  para que vivan tu y tus descendientes."<<endl;
 }
